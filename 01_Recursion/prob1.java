a1da83a1e71019a72eacd043815d77eeb38e50c6
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-07-28 10:06:52 +0000

    prob1

--- 01_Recursion/prob1.java
+++ 01_Recursion/prob1.java
@@ -4,7 +4,8 @@ public class prob1 {
     static void printName(String name) {
         if (count == 5)
             return;
-        System.out.println(name);
+        System.out.println(name+
+        (count+1));
         count++;
         printName(name);
         
