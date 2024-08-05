e7849e0725d6d08a7d11a2b1f5f14f383b109126
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-08-05 14:33:36 +0000

    bubble sort

---
+++ 02_Sorting/BubbleSort.java
@@ -0,0 +1,21 @@
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    static void sortNum(int[] arr, int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n-1; j++) {
+                while (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = { 5, 2, 1, 3, 0, 4 };
+        sortNum(arr, arr.length);
+        System.out.println(Arrays.toString(arr));
+    }
+}
