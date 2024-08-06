73f834e630d50b668e78c4d8b054db065fe2c3d5
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-08-06 10:25:44 +0000

    Insertion sort

--- 02_Sorting/InsertionSort.java
+++ 02_Sorting/InsertionSort.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 
 public class InsertionSort {
     static void sort(int[] num, int n) {
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n-1; i++) {
             for (int j = i ; j > 0; j--) {
                 while (num[j] < num[j - 1]) {
                     int temp = num[j];
