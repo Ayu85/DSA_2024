8ed97fc9976dc8cfa1f8bfd5548e37762e98d46f
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-08-06 10:01:37 +0000

    insertion sort

--- 02_Sorting/BubbleSort.java
+++ 02_Sorting/BubbleSort.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 
 public class BubbleSort {
     static void sortNum(int[] arr, int n) {
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n-1; i++) {
             boolean IsSwap = false;
             for (int j = 0; j < n - 1; j++) {
                 while (arr[j] > arr[j + 1]) {
---
+++ 02_Sorting/InsertionSort.java
@@ -0,0 +1,23 @@
+
+import java.util.Arrays;
+
+public class InsertionSort {
+    static void sort(int[] num, int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = i ; j > 0; j--) {
+                while (num[j] < num[j - 1]) {
+                    int temp = num[j];
+                    num[j] = num[j - 1];
+                    num[j - 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 3, 5, 2, 0, 1, 4 };
+        int[] arr2 = { 14, 9, 15, 12, 6, 8,13 };
+        sort(arr2, arr2.length);
+        System.out.println(Arrays.toString(arr2));
+    }
+}
