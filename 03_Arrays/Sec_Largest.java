ed03837eece59943f94f5db242eab9e82fc3dc2f
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-09-03 10:14:24 +0000

    optimal

---
+++ 03_Arrays/Sec_Largest.java
@@ -0,0 +1,20 @@
+public class Sec_Largest {
+    static int getSecLargest(int[] num) {
+        int max = num[0];
+        int smax = num[0];
+        for (int i = 1; i < num.length; i++) {
+            if (num[i] > max) {
+                smax = max;
+                max = num[i];
+            } else {
+                if (num[i] < max && num[i] > smax)
+                    smax = num[i];
+            }
+        }
+        return smax;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getSecLargest(new int[] { 1, 2, 4, 7, 7, 5 }));
+    }
+}
