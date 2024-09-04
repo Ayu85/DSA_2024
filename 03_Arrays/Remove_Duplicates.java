cc737a5c9c68f685663bf1ff5aa0d888cd16bdad
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-09-04 07:17:43 +0000

    better

--- 03_Arrays/Remove_Duplicates.java
+++ 03_Arrays/Remove_Duplicates.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 
 public class Remove_Duplicates {
-    static void removeDup(int[] arr) {
+    static int removeDup(int[] arr) {
         int j = 0;
         for (int i = 0; i < arr.length-1; i++) {
             if (arr[i] != arr[i + 1]) {
@@ -9,12 +9,13 @@ public class Remove_Duplicates {
                 arr[j] = arr[i + 1];
             }
         }
+        return j;
     }
 
     public static void main(String[] args) {
         int[] arr = { 1, 1, 2, 2, 2, 3, 3 };
         int[] arr2 = { 3, 3, 4, 5, 5, 8, 8 };
-        removeDup(arr2);
+        System.out.println(removeDup(arr2));
         System.out.println(Arrays.toString(arr2));
     }
 }
