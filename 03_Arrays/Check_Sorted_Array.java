70b7b2e08e26f8573560e7f25e85e8fa4590afea
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-09-04 06:57:02 +0000

    better

--- 03_Arrays/Check_Sorted_Array.java
+++ 03_Arrays/Check_Sorted_Array.java
@@ -1,15 +1,17 @@
 public class Check_Sorted_Array {
-    static boolean checkArr(int[] arr){
-        boolean isSorted=true;
-        for(int i=0;i<arr.length-1;i++){
-            if(arr[i]>arr[i+1])
-            isSorted=false;
+    static boolean checkArr(int[] arr) {
+        boolean isSorted = true;
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                isSorted = false;
+                return isSorted;
+            }
         }
         return isSorted;
     }
 
     public static void main(String[] args) {
-        System.out.println(checkArr(new int[] {1,2,3,3,4
+        System.out.println(checkArr(new int[] { 1, 20, 3, 3, 4
         }));
     }
 }
