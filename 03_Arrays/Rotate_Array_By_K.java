5d764323bd8a0ce73da38c0f01e8b6707d785282
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-09-06 04:18:44 +0000

    brute

--- 03_Arrays/Rotate_Array_By_K.java
+++ 03_Arrays/Rotate_Array_By_K.java
@@ -1,3 +1,24 @@
+import java.util.Arrays;
+
 public class Rotate_Array_By_K {
-    
+    static void rotate(int[] arr, int k) {
+        int temp[] = new int[k];
+        for (int i = 0; i < k; i++) {
+            temp[i] = arr[i];
+        }
+        for (int i = k; i < arr.length; i++) {
+            arr[i-k] = arr[i];
+        }
+        int j = 0;
+        for (int i = arr.length - k; i < arr.length; i++) {
+            arr[i] = temp[j];
+            j++;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5 };
+        rotate(arr, 2);
+        System.out.println(Arrays.toString(arr));
+    }
 }
