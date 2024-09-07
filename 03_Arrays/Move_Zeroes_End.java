ebab264bf0dcc48a4386f6443deef68f4080f98b
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-09-07 03:24:05 +0000

    better

--- 03_Arrays/Move_Zeroes_End.java
+++ 03_Arrays/Move_Zeroes_End.java
@@ -10,16 +10,30 @@ public class Move_Zeroes_End {
                 temp[j++] = arr[i];
             }
         }
-        for(int i=j;i<n;i++){
-            temp[i]=0;
+        for (int i = j; i < n; i++) {
+            temp[i] = 0;
         }
-        for(int i=0;i<n;i++){
-            arr[i]=temp[i];
+        for (int i = 0; i < n; i++) {
+            arr[i] = temp[i];
         }
     }
-    public static void main(String[] args){
-        int[] arr={1,0,2,3,2,0,0,4,5,1};
-        move_brute(arr, arr.length);
+
+    static void move_better(int[] arr, int n) {
+        int j = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] != 0) {
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+                j++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 0, 2, 3, 2, 0, 0, 4, 5, 1 };
+        int[] arr2={0,1,0,3,12};
+        move_better(arr, arr.length);
         System.out.println(Arrays.toString(arr));
     }
 }
