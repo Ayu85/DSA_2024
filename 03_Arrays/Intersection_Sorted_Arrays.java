236712978c1846136d80ecc8c4c099fee785c2d2
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-09-08 11:18:06 +0000

    optimal

---
+++ 03_Arrays/Intersection_Sorted_Arrays.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Intersection_Sorted_Arrays {
+    static ArrayList<Integer> intersection(int[] arr1, int[] arr2) {
+        int n = arr1.length;
+        int m = arr2.length;
+        int i = 0;
+        int j = 0;
+        ArrayList<Integer> temp = new ArrayList<>();
+        while (i < n && j < m) {
+            if (arr1[i] < arr2[j])
+                i++;
+            else if (arr2[j] < arr1[i])
+                j++;
+            else {
+                temp.add(arr1[i]);
+                i++;
+                j++;
+            }
+        }
+        return temp;
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = { 1, 2, 2, 3, 3, 4, 5, 6 };
+        int[] arr2 = { 2, 3, 3, 5, 6, 6, 7 };
+        ArrayList<Integer> temp = intersection(arr1, arr2);
+        for (Integer i : temp) {
+            System.out.print(i + " ");
+        }
+    }
+}
