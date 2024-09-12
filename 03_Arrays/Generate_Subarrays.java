f88daf69ebd6d5718f547ad31288b387d829742c
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-09-12 03:26:56 +0000

    brute force

--- 03_Arrays/Generate_Subarrays.java
+++ 03_Arrays/Generate_Subarrays.java
@@ -1,12 +1,22 @@
 public class Generate_Subarrays {
     public static void main(String[] args) {
-        int[] arr = { 1, 1, 2, 3, 3, 4, 4 };
+        int[] arr = { 1, 2, 3, 1, 1, 1, 1, 4, 2, 3 };
+        int maxl = 0;
+        int givenSum = 3;
         for (int i = 0; i < arr.length; i++) {
             for (int j = i + 1; j < arr.length; j++) {
-                for (int k = i; k <= j; k++)
+                int sum = 0;
+                int length = 0;
+                for (int k = i; k <= j; k++) {
+                    sum += arr[k];
+                    length++;
                     System.out.print(arr[k] + " ");
-                System.out.println();
+                }
+                if (sum == givenSum && length > maxl)
+                    maxl = length;
+                System.out.println("sum- " + sum);
             }
         }
+        System.out.println("Answer-" + maxl);
     }
 }
