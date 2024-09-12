bc3d78fe652b2983798f54ee113f954ca55190f8
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-09-12 03:32:43 +0000

    better

--- 03_Arrays/Generate_Subarrays.java
+++ 03_Arrays/Generate_Subarrays.java
@@ -1,4 +1,19 @@
 public class Generate_Subarrays {
+    static int better(int[] arr, int n) {
+        int maxl = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int sum = 0;
+            int length = 0;
+            for (int j = i + 1; j < arr.length; j++) {
+                sum += arr[j];
+                length++;
+                if (sum == n && length > maxl)
+                    maxl = length;
+            }
+        }
+        return maxl;
+    }
+
     public static void main(String[] args) {
         int[] arr = { 1, 2, 3, 1, 1, 1, 1, 4, 2, 3 };
         int maxl = 0;
@@ -17,6 +32,7 @@ public class Generate_Subarrays {
                 System.out.println("sum- " + sum);
             }
         }
-        System.out.println("Answer-" + maxl);
+        // System.out.println("Answer-" + maxl);
+        System.out.println(better(arr, 3));
     }
 }
